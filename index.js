1d6081714a5bd3087d11b01ddc9e9b901939e940
Kali Cano <158784747+kalicano@users.noreply.github.com>
2024-02-07 18:20:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Sahip Check","Türkiyenin En Sağlam Botu :)"];
 
 
 let currentIndex = 0;
